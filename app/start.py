f8bac5642234d2d99a40e10ecd16b8fbb84ffa84
WesRoach <wesr000@gmail.com>
2018-11-28 19:49:48 -0600

    Black default code formatter

--- app/start.py
+++ app/start.py
@@ -4,10 +4,10 @@ import random
 
 
 # stacks consist of cards ascending/descending
-class Stack():
+class Stack:
     def __init__(self, order):
         self.order = order
-        self.cards = [1 if order == 'asc' else 100]
+        self.cards = [1 if order == "asc" else 100]
 
     def getOrder(self):
         return self.order
@@ -19,7 +19,7 @@ class Stack():
         return self.cards[-1]
 
     def placeCard(self, card):
-        if self.getOrder() == 'asc':
+        if self.getOrder() == "asc":
             if card > self.getTopCard():
                 self.cards.append(card)
                 return True
@@ -34,17 +34,17 @@ class Stack():
 
 
 # board is made of four stacks
-class Board():
+class Board:
     def __init__(self):
-        self.stacks = [Stack('asc') for i in range(0, 2)]
-        self.stacks.extend([Stack('desc') for i in range(0, 2)])
+        self.stacks = [Stack("asc") for i in range(0, 2)]
+        self.stacks.extend([Stack("desc") for i in range(0, 2)])
 
     def getStacks(self):
         return self.stacks
 
 
 # deck is shuffled cards 2 through 99
-class Deck():
+class Deck:
     def __init__(self):
         self.deck = [i for i in range(2, 100)]
         random.shuffle(self.deck)
@@ -53,7 +53,7 @@ class Deck():
 
     # What cards are available in Deck
     def getRemainingCards(self):
-        return self.deck[self.topCard:]
+        return self.deck[self.topCard :]
 
     def getOriginalCards(self):
         return self.deck
@@ -63,19 +63,19 @@ class Deck():
 
     # Remove N cards from deck and return it
     def drawCards(self, n: int):
-        draw_cards = self.deck[self.topCard:(self.topCard + int(n))]
+        draw_cards = self.deck[self.topCard : (self.topCard + int(n))]
         self.topCard += int(n)
         return draw_cards
 
 
 # player's hand contains cards
-class Player():
+class Player:
     def __init__(self):
         self.hand = []
 
 
 # Game logic
-class Game():
+class Game:
     def __init__(self, num_players, cards_per_player):
         self.players = [Player() for _ in range(0, int(num_players))]
         # lay down the board and shuffle the deck
@@ -106,22 +106,21 @@ class Game():
 
         return selected_card, selected_stack
 
-
     def play(self):
         while self.deck.getRemainingCardsCount() > 0:
             # Print Game state
             print("\n=== The Game ===")
-            print('Cards in Deck: %s' % len(self.deck.getRemainingCards()) )
-            print('-- Board --')
+            print("Cards in Deck: %s" % len(self.deck.getRemainingCards()))
+            print("-- Board --")
             for idx, stack in enumerate(self.board.getStacks()):
                 _topCard = stack.getTopCard()
                 _order = stack.getOrder()
-                print(f'[{idx + 1}] {_order} Top Card: {_topCard}')
+                print(f"[{idx + 1}] {_order} Top Card: {_topCard}")
 
             for idx, player in enumerate(self.players):
-                print(f'-- Player {idx}\'s Hand --')
+                print(f"-- Player {idx}'s Hand --")
                 for idx, card in enumerate(player.hand):
-                    print(f'[{idx + 1}] {card}')
+                    print(f"[{idx + 1}] {card}")
 
             # Player Options
             print("")
@@ -132,7 +131,7 @@ class Game():
             card = self.players[0].hand[selected_card - 1]
 
             # try to place card on stack
-            if (self.board.stacks[selected_stack - 1].placeCard(card)):
+            if self.board.stacks[selected_stack - 1].placeCard(card):
                 # if card placed - remove from player's hand
                 self.players[0].hand.remove(card)
 
